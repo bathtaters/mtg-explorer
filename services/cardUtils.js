ec8106955943321254f3765027b7f021bb8e8d35
Nick C <bathtaters@gmail.com>
2024-07-29 21:52:10 -0400

    fix non-standard characters

--- services/cardUtils.js
+++ services/cardUtils.js
@@ -9,12 +9,18 @@ const toCards = (cards,token) => {
   return cards;
 };
 
+// Replace annoying characters
+const charFix = (s) => s
+  .replace(/[\u2018\u2019]/g, "'") // Single smart quotes
+  .replace(/[\u201C\u201D]/g, '"') // Double smart quotes
+  .replace(/\u2013|\u2014/g, "-"); // Long dashes
+
 const appendTokens = (newTokens, cardsObj) => newTokens.reduce(toCards, cardsObj || {});
 
 // Normalize card name for looking up files
 const cardFilename = cardName => cardName.replace(/\s+/g,'-').replace(/[^\w-]/g,'').toLowerCase() + '.json';
 
 // Sanitize user input
-const sanitizeCards = cards => cards.split('\n').map(s=>s.replace(/^\s*\d+\s/,'').trim()).filter(s=>!!s);
+const sanitizeCards = cards => cards.split('\n').map(s=>charFix(s.replace(/^\s*\d+\s/,'').trim())).filter(s=>!!s);
 
 module.exports = { appendTokens, cardFilename, sanitizeCards }
